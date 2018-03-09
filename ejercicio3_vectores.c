ce28016721a59ed7b4fe58ace7f12144f93dfc9c
paula-arellano <36606152+paula-arellano@users.noreply.github.com>
2018-03-09 16:25:02 +0100

    ejercicio3_vectores

--- ejercicio3_vectores.c
+++ ejercicio3_vectores.c
@@ -2,11 +2,11 @@
 /*Haz un programa que almacene en un vector el factorial de los numeros entre 0 y 10 y se pueda 
 preguntar al usuario que escoja de que numero quiere pedir el factorial, para lo cual se consultara simplemente
 el vector e imprimira el resultado. El programa debe indicar un mensaje de error si el numero no esta en el rango 
-de 0 a 10 y pedirá otro número*/
+de 0 a 10 y pedirÃ¡ otro nÃºmero*/
 
 #include<stdio.h>
 void main() {
-	int factorial[11] = { 0,1,1,1,1,1,1,1,1,1,1 };
+	int factorial[11];
 	int i, numero;
 
 	/*for (i = 0; i < 11; i++) {
@@ -17,7 +17,10 @@ void main() {
 		}
 	} */
 
-	for (i = 2; i < 10; i++) {
+	for (i = 0; i < 10; i++) {
+		if (i==0 || i==1)
+			factorial[i]=i;
+		else
 		factorial[i] = factorial[i-1] * i;
 	}
 
