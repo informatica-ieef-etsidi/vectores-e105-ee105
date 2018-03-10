d28fcc73cb178dff823aee55cacd4f2a25dbf0e6
ismagp99 <36692778+ismagp99@users.noreply.github.com>
2018-03-10 17:48:28 +0100

    Add files via upload

---
+++ Ejercicio5.c
@@ -0,0 +1,31 @@
+//Programa que pide el valor de 20 datos de un vector y un numero cualquiera para comprobar si esta incluido
+#include <stdio.h>
+
+void main() {
+	int i, n;
+	int contieneN = 0;
+	int vector[20];
+	for (i = 0; i < 20; i++) {
+		printf("Introduzca la coordenada %d del vector\n", i + 1);
+		scanf_s("%d", &vector[i]);
+	}
+	printf("Introduzca un numero para ver si esta contenido en el vector\n");
+	scanf_s("%d", &n);
+	system("cls");
+	printf("Su vector es (");
+	for (i = 0;i < 19;i++) {
+		printf("%d, ", vector[i]);
+	}
+	printf("%d)\n", vector[19]);
+	for (i = 0; i < 20; i++) {
+		if (vector[i] == n){
+			printf("El numero %d esta contenido en el vector\n", n);
+			contieneN = 1;
+			break;
+		}
+	}
+	if (contieneN == 0) {
+		printf("El numero %d no esta contenido en el vector\n", n);
+	}
+	system("pause");
+}
